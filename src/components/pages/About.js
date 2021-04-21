09157ca4bbe6bec0dc7745b0c035bd175f177a02
Hieu Le <hieu.le@student.ufv.ca>
2021-04-21 04:33:35 -0700

    Added about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,16 +1,63 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const About = () => {
   return (
-    <div className='text-center mt-5'>
-      <h1>About This App</h1>
-      <h4>Small Talks</h4>
-      <p className='lead'>
-        <strong>Version: </strong>1.0.0
-      </p>
-      <p>Copyright. All Right Reserved! </p>
-    </div>
+    <Fragment>
+      <header className='text-center py-5 mb-4 mt-5' style={backgroundStyle}>
+        <div className='container'>
+          <h1 className='font-weight-light text-white'>Meet the Team</h1>
+        </div>
+      </header>
+
+      <div className='row'>
+        <div className='col-xl-6 col-md-6 mb-4'>
+          <div className='card border-0 shadow'>
+            <img
+              src='https://hieulocker.tk/images/hieu.jpg'
+              class='card-img-top rounded-circle'
+              alt='...'
+            />
+            <div className='card-body text-center'>
+              <h5 className='card-title mb-0'>Team member</h5>
+              <div className='card-text text-black-50'>
+                My name is Hieu Le. I am the third year student in Bachelor of
+                Computer Information System at the University of the Fraser
+                Valley, concerntrating on Software Development. In my spare
+                time, I like playing Cities Skylines, GTAV, reading techblogs
+                and travelling.
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='col-xl-6 col-md-6 mb-4'>
+          <div className='card border-0 shadow'>
+            <img
+              src='https://gurjitlocker.tk/images/profile.jpg'
+              class='card-img-top rounded-circle'
+              alt='...'
+            />
+            <div className='card-body text-center'>
+              <h5 className='card-title mb-0'>Team member</h5>
+              <div className='card-text text-black-50'>
+                I am Gurjit! A 3rd year Computer Information Systems student at
+                UFV, majoring in Software Development and an aspiring Software
+                Developer! I love exploring new places, reading tech blogs, and
+                talking to new people {':)'} My expertise is in {'=>'} Java,
+                JavaScript, Node, React, SQL, and Python!
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
+};
+
+const backgroundStyle = {
+  background:
+    'linear-gradient(to left, rgba(106, 17, 203, 0.55), rgba(37, 117, 252, 0.55))',
+  borderRadius: '20px',
 };
 
 export default About;
