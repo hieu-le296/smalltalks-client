50a9237402ea6b035bd380fec937cda483b73d72
Hieu Le <hieu.le@student.ufv.ca>
2021-04-21 03:49:23 -0700

    Change Navbar UI and added profile pic

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import AuthContext from '../../context/auth/authContext';
 
-const Navbar = ({ icon }) => {
+const API_URL = 'http://datacomputation.com/uploads';
+
+const Navbar = ({ icon, profilePic }) => {
   const authContext = useContext(AuthContext);
   const { isAuthenticated, logout, user, loadUser } = authContext;
 
@@ -19,19 +21,48 @@ const Navbar = ({ icon }) => {
   const authLinks = (
     <Fragment>
       <li className='nav-item'>
-        <Link to='/dashboard' className='nav-link text-white'>
+        <Link to='/dashboard' className='nav-link'>
           Dashboard
         </Link>
       </li>
-      <li className='nav-item'>
-        <Link to='/profile' className='nav-link text-white'>
-          Profile
-        </Link>
-      </li>
-      <li className='nav-item'>
-        <a onClick={onLogout} href='#!' className='nav-link text-white'>
-          <i className='fas fa-sign-out-alt ' /> <span>Logout</span>
+
+      <li className='nav-item dropdown'>
+        <a
+          className='nav-link dropdown-toggle d-flex align-items-center'
+          href='#'
+          id='navbarDropdownMenuLink'
+          role='button'
+          data-mdb-toggle='dropdown'
+          aria-expanded='false'
+        >
+          <img
+            src={user && `${API_URL}/${user.data.profilePic}`}
+            className='rounded-circle'
+            height='22'
+            alt=''
+            loading='lazy'
+            onError={(e) => (e.style.visibility = 'hidden')}
+          />
         </a>
+        <ul
+          className='dropdown-menu'
+          id='dropDown'
+          aria-labelledby='navbarDropdownMenuLink'
+        >
+          <li className='dropdown-item'>Hello {user && user.data.name}</li>
+          <li className='dropdown-item nav-link text-black '>
+            <i className='fas fa-user-alt ' />{' '}
+            <Link to='/profile' className='text-black'>
+              My Profile
+            </Link>
+          </li>
+
+          <li className='nav-item '>
+            <a onClick={onLogout} href='#!' className='nav-link text-black '>
+              <i className='fas fa-sign-out-alt' /> <span>Logout</span>
+            </a>
+          </li>
+        </ul>
       </li>
     </Fragment>
   );
@@ -39,12 +70,12 @@ const Navbar = ({ icon }) => {
   const guestLinks = (
     <Fragment>
       <li className='nav-item'>
-        <Link to='/register' className='nav-link text-white'>
+        <Link to='/register' className='nav-link'>
           Register
         </Link>
       </li>
       <li className='nav-item'>
-        <Link to='/login' className='nav-link text-white'>
+        <Link to='/login' className='nav-link'>
           Login
         </Link>
       </li>
@@ -52,8 +83,17 @@ const Navbar = ({ icon }) => {
   );
 
   return (
-    <nav className='navbar navbar-expand-lg bg-primary'>
+    <nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
       <div className='container-fluid '>
+        <Link to='/' className='navbar-brand text-white'>
+          <h4 className='ms-3'>
+            <i className={icon} /> Small Talks
+          </h4>
+        </Link>
+        <Link to='/about' className='nav-link text-white'>
+          <h6>About</h6>
+        </Link>
+
         <button
           className='navbar-toggler'
           type='button'
@@ -70,23 +110,14 @@ const Navbar = ({ icon }) => {
           className='collapse navbar-collapse justify-content-center'
           id='navbarCenteredExample'
         >
-          <ul className='navbar-nav mb-2 mb-lg-0'>
+          <ul className='navbar-nav ms-auto me-5'>
             <li className='nav-item'>
-              <Link to='/' className='navbar-brand text-white'>
-                <i className={icon}></i>
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link to='/' className='nav-link text-white'>
+              <Link to='/' className='nav-link'>
                 Home
               </Link>
             </li>
+
             {isAuthenticated ? authLinks : guestLinks}
-            <li className='nav-item'>
-              <Link to='/about' className='nav-link text-white'>
-                About
-              </Link>
-            </li>
           </ul>
         </div>
       </div>
@@ -95,11 +126,12 @@ const Navbar = ({ icon }) => {
 };
 
 Navbar.propTypes = {
-  icon: PropTypes.string,
+  icon: PropTypes.string.isRequired,
 };
 
 Navbar.defaultProps = {
   icon: 'fas fa-comments fa-lg',
+  profilePic: 'http://datacomputation.com/uploads/default.jpeg',
 };
 
 export default Navbar;
