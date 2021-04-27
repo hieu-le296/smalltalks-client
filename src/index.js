f5880989b5f723794757eb06742f77c37a7d61ef
Hieu Le <hieu.le@student.ufv.ca>
2021-04-26 22:43:51 -0700

    Removed mdb-ui-kit, added its cdn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'mdb-ui-kit/css/mdb.min.css';
-import 'mdb-ui-kit/js/mdb.min';
 
 import './index.css';
 import App from './App';
