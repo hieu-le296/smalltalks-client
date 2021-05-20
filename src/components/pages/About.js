7b3f8732992a4dee3b948b1db2e348421957a439
Hieu <hieu.le@student.ufv.ca>
2021-05-20 03:22:36 -0700

    Sending contact form to backend for email

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,48 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useContext } from 'react';
+import axios from 'axios';
+import AlertContext from '../../context/alert/alertContext';
 
 const About = () => {
+  const alertContext = useContext(AlertContext);
+  const { setAlert } = alertContext;
+
+  const [contact, setContact] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const { name, email, message } = contact;
+
+  const onChange = (e) => {
+    setContact({ ...contact, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    try {
+      const res = await axios.post(
+        `${process.env.REACT_APP_API_URL}/api/v1/contactme`,
+        contact,
+        config
+      );
+      setAlert(res.data.msg, 'success');
+      setContact({
+        name: '',
+        email: '',
+        message: '',
+      });
+    } catch (err) {
+      setAlert(err.response.data.error, 'danger');
+    }
+  };
+
   return (
     <Fragment>
       <header className='text-center py-5 mb-4 mt-5' style={backgroundStyle}>
@@ -13,13 +55,16 @@ const About = () => {
       </header>
 
       <div className='row'>
-        <div className='col-xl-6 col-md-6 mb-4'>
-          <div className='card border-0 shadow'>
+        <div className='col-xl-6 col-md-4 mb-auto'>
+          <div
+            className='card mt-3 border-0 shadow'
+            style={{ height: '400px', width: '550px' }}
+          >
             <img
               src='https://hieulocker.tk/images/hieu.jpg'
               className='card-img-top rounded-circle mx-auto mt-3'
               alt='...'
-              style={{ maxHeight: '50%', maxWidth: '50%' }}
+              style={{ maxHeight: '50%', maxWidth: '35%' }}
             />
             <div className='card-body text-center'>
               <h5 className='card-title mb-0'>Team member</h5>
@@ -34,24 +79,63 @@ const About = () => {
           </div>
         </div>
 
-        <div className='col-xl-6 col-md-6 mb-4'>
-          <div className='card border-0 shadow'>
-            <img
-              src='https://gurjitlocker.tk/images/profile.jpg'
-              className='card-img-top rounded-circle mx-auto mt-3'
-              alt='...'
-              style={{ maxHeight: '50%', maxWidth: '50%' }}
-            />
-            <div className='card-body text-center'>
-              <h5 className='card-title mb-0'>Team member</h5>
-              <div className='card-text text-black-50'>
-                I am Gurjit! A 3rd year Computer Information Systems student at
-                UFV, majoring in Software Development and an aspiring Software
-                Developer! I love exploring new places, reading tech blogs, and
-                talking to new people {':)'} My expertise is in {'=>'} Java,
-                JavaScript, Node, React, SQL, and Python!
+        <div className='col-xl-6 col-md-8 mb-auto'>
+          <div
+            className='mt-3 card form-container'
+            style={{ height: '400px', width: '550px' }}
+          >
+            <h4 className='text-center mt-3'>Contact Me</h4>
+            <form onSubmit={onSubmit}>
+              <div className=' mb-4'>
+                <label className='form-label' htmlFor='name'>
+                  Name
+                </label>
+                <input
+                  type='text'
+                  id='name'
+                  className='form-control'
+                  name='name'
+                  value={name}
+                  onChange={onChange}
+                />
               </div>
-            </div>
+
+              <div className='mb-4'>
+                <label className='form-label' htmlFor='email'>
+                  Email address
+                </label>
+                <input
+                  type='email'
+                  id='email'
+                  className='form-control'
+                  name='email'
+                  value={email}
+                  onChange={onChange}
+                />
+              </div>
+
+              <div className='mb-4'>
+                <label className='form-label' htmlFor='message'>
+                  Message
+                </label>
+                <textarea
+                  className='form-control'
+                  style={{ overflow: 'auto' }}
+                  id='message'
+                  rows='2'
+                  name='message'
+                  value={message}
+                  onChange={onChange}
+                ></textarea>
+              </div>
+
+              <button
+                type='submit'
+                className='btn btn-primary btn-rounded btn-block mb-4'
+              >
+                Send
+              </button>
+            </form>
           </div>
         </div>
       </div>
